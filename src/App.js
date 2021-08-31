0495b065bde7df2111e73bb4033a2be9dc35e15f
KaterinaGerts <75572090+KaterinaGerts@users.noreply.github.com>
2021-08-31 18:35:44 +0300

    внесла корректно данные

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ import Filter from 'components/Filter/Filter';
 import { v4 as uuidv4 } from 'uuid';
 
 function App() {
-  const [contacts, setContacts] = useState({
-    contacts: [
+  const [contacts, setContacts] = useState(
+   [
       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
-  });
+  );
   const [filter, setFilter] = useState('');
 
   // const formSubmitHandler = data => {
@@ -34,9 +34,9 @@ function App() {
 
     checkedName
       ? alert(`${name} is already in contacts!`)
-      : setContacts(({ contacts }) => ({
-          contacts: [...contacts, data],
-        }));
+      : setContacts(( contacts ) => (
+          [...contacts, data]
+        ));
   };
 
   const deleteContact = contactId => {
