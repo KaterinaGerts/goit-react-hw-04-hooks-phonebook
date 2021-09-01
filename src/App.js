241344ca6b5fdf4dacb22f47feb183f05a4c96c2
KaterinaGerts <75572090+KaterinaGerts@users.noreply.github.com>
2021-09-01 16:36:48 +0300

    вынесла локалсторидж в отдельный хук

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
+import useLocalStorage from 'hooks/useLocalStorage';
 import Container from 'components/Container/Container';
 import ContactForm from 'components/ContactForm/ContactForm';
 import ContactList from 'components/ContactList/ContactList';
@@ -6,17 +7,9 @@ import Filter from 'components/Filter/Filter';
 import { v4 as uuidv4 } from 'uuid';
 
 function App() {
-  const [contacts, setContacts] = useState(() => { return JSON.parse(window.localStorage.getItem('contacts')) ?? [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]});
-  const [filter, setFilter] = useState('');
+  const [contacts, setContacts] = useLocalStorage('contacts');
 
-  useEffect(() => {
-    window.localStorage.setItem('contacts', JSON.stringify(contacts));   
-  }, [contacts])
+  const [filter, setFilter] = useState('');
 
   const addContact = ({ name, number }) => {
     const data = {
